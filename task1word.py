5cad69832f1a18f18bde79b6c8489616b45c0846
usamakniazi <32064934+usamakniazi@users.noreply.github.com>
2017-10-20 16:04:25 -0300

    update task1word.py

--- task1word.py
+++ task1word.py
@@ -8,8 +8,8 @@ string = l[0]
 stringsplit = string.split()
 file1 = []
    for i in stringsplit:
-   file1.append(len(i))
-   e = max(file1)
+      file1.append(len(i))
+      e = max(file1)
    for j in stringsplit:
-     if len(j) == e:
- print("The longest word is", j, "and it is", len(j),"characters long")
+   if len(j) == e:
+print("The longest word is", j, "and it is", len(j),"characters long")
